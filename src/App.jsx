2b5bb0ccffc134a942ae8bc8e4735058c72c9167
Naman Muley <naman.g.muley@gmail.com>
2025-07-24 22:58:38 +0530

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProjectList from "./components/ProjectList.jsx";
+
+function App() {
+  return (
+    <div className="dashboard">
+      <h1>Project Dashboard</h1>
+      <ProjectList />
+    </div>
+  );
+}
+
+export default App;
